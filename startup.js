3f24e82cab79b0e64a24edb8f731b57417aa85f0
kidalonious <120538119+kidalonious@users.noreply.github.com>
2024-02-28 12:40:09 -0700

    changeText function implemented

--- startup.js
+++ startup.js
@@ -4,6 +4,15 @@ function login() {
     window.location.href = "index.html";
   }
 
-function changeText() {
-    
+  function changeText() {
+    var header = document.querySelector('h4');
+    var button = document.getElementById('Prompt options');
+
+    if (header.textContent.trim() === "The prompt for the day will go here (third party call to writing prompts)") {
+        header.textContent = "Free Write";
+        button.textContent = "Switch to Prompt";
+    } else {
+        header.textContent = "The prompt for the day will go here (third party call to writing prompts)";
+        button.textContent = "Switch to Free Write";
+    }
 }
