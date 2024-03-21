2e19d8120e51cc8ff8adbbb2fec830540c07df08
kidalonious <120538119+kidalonious@users.noreply.github.com>
2024-03-20 21:53:26 -0600

    More changes

--- database.js
+++ database.js
@@ -5,9 +5,9 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('promptr');
+const db = client.db('simon');
 const userCollection = db.collection('user');
-const submissionCollection = db.collection('submission');
+const scoreCollection = db.collection('score');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -20,6 +20,10 @@ const submissionCollection = db.collection('submission');
 
 function getUser(email) {
   return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
 }
 
 async function createUser(email, password) {
@@ -35,3 +39,25 @@ async function createUser(email, password) {
 
   return user;
 }
+
+function addScore(score) {
+  scoreCollection.insertOne(score);
+}
+
+function getHighScores() {
+  const query = { score: { $gt: 0, $lt: 900 } };
+  const options = {
+    sort: { score: -1 },
+    limit: 10,
+  };
+  const cursor = scoreCollection.find(query, options);
+  return cursor.toArray();
+}
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  createUser,
+  addScore,
+  getHighScores,
+};
